f90cda4ba4ff39ce898c55e4612ebda775c35bbd
Serenity <dualz.nz@gmail.com>
2020-07-13 12:31:02 +1200

    added in alerts.blade.php but noticed that cause the way the theme is laid out i have to include it in each blade view which is not the end of the world but is annoying but this also means i can choose views to push the alerts to instead of having it globally defined in the main.blade.php view

---
+++ resources/views/layouts/page-template.blade.php
@@ -0,0 +1,42 @@
+@section('title')
+    PAGETITLE
+@endsection
+@extends('layouts.main')
+@section('rightbar-content')
+    <!-- Start XP Breadcrumbbar -->
+    <div class="xp-breadcrumbbar text-center">
+        <h4 class="page-title">PAGETITLE</h4>
+        <ol class="breadcrumb">
+            <li class="breadcrumb-item"><a href="{{ route('dashboard') }}">INDEX</a></li>
+            <li class="breadcrumb-item active" aria-current="page">PAGE</li>
+        </ol>
+    </div>
+    <!-- End XP Breadcrumbbar -->
+    <!-- Start XP Contentbar -->
+    <div class="xp-contentbar">
+        <!-- Start Include Alerts -->
+        @include('layouts.alerts')
+        <!-- End Inclue Alerts -->
+        <!-- Start Row -->
+        <div class="row">
+            <!-- Start XP Col -->
+            <div class="col-lg-12">
+                <!-- Start Card -->
+                <div class="card m-b-30">
+                    <div class="card-header bg-white">
+                        <h5 class="card-title text-black">PAGETITLE</h5>
+                        <h6 class="card-subtitle">SUBTITLE</h6>
+                    </div>
+                    <div class="card-body">
+
+
+                    </div>
+                </div>
+                <!-- End Card -->
+            </div>
+            <!-- End XP Col -->
+        </div>
+        <!-- End Row -->
+    </div>
+    <!-- End XP Contentbar -->
+@endsection
