0de4adc75013304d35244271ae4a8a3f956cda94
Serenity <dualz.nz@gmail.com>
2020-08-19 14:23:05 +1200

    another update to db travis

--- config/database.php
+++ config/database.php
@@ -94,7 +94,7 @@ return [
         'testing' => [
             'driver' => 'mysql',
             'host' => env('DB_TEST_HOST', 'localhost'),
-            'database' => env('DB_TEST_DATABASE', 'manager-testing'),
+            'database' => env('DB_TEST_DATABASE', 'testing'),
             'username' => env('DB_TEST_USERNAME', 'homestead'),
             'password' => env('DB_TEST_PASSWORD', 'secret'),
             'charset' => 'utf8',
